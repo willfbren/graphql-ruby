9361306ef29919873e96f50886c636df78821457
willfbren <willfbren@gmail.com>
2020-12-23 18:26:56 -0600

    initial commit

---
+++ app/graphql/mutations/create_link.rb
@@ -0,0 +1,14 @@
+module Mutations
+    class CreateLink < BaseMutation
+        argument :description, String, required: true
+        argument :url, String, required: true
+        type Types::LinkType
+
+        def resolve(description: nil, url: nil)
+            Link.create!(
+                description: description,
+                url: url,
+            )
+        end
+    end
+end
